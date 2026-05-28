834a3d70044a656c3b1c861f5b3461cd0ee1f06d
Nomanur Rahman <nomanurrahman@gmail.com>
2026-05-28 21:44:33 +0600

    composer test added

--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
-Artisan::command('inspire', function () {
+Artisan::command('inspire', function (): void {
+    /** @phpstan-ignore-next-line */
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
