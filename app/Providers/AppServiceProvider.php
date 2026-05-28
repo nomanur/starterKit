7ceb55e0d0d3aeb737e089da2744b037f95cbf96
Nomanur Rahman <nomanurrahman@gmail.com>
2026-05-28 21:12:49 +0600

    appserviceprovider updated

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use Carbon\CarbonImmutable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,12 +24,26 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->configureModels();
+        $this->configureCommands();
+        $this->configureDates();
     }
 
     protected function configureModels(): void
     {
         Model::shouldBeStrict(); //prevents accessing missing attributes n Prevent lazy loading
 
+        Model::unguard();
+    }
 
+    private function configureCommands(): void
+    {
+        DB::prohibitDestructiveCommands(
+            $this->app->isProduction(),
+        );
+    }
+
+    private function configureDates(): void
+    {
+        Date::use(CarbonImmutable::class);   
     }
 }
