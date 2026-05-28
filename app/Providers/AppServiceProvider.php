da99b61dba88159c97ab2cda038f3ffc6acebd64
Nomanur Rahman <nomanurrahman@gmail.com>
2026-05-28 20:56:36 +0600

    first commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        $this->configureModels();
+    }
+
+    protected function configureModels(): void
+    {
+        Model::shouldBeStrict(); //prevents accessing missing attributes n Prevent lazy loading
+
+
+    }
+}
